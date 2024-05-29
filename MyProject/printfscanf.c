57aa0563ee3eecebfb1f02e8d755e480477fb0f0
qoxow <qoxowkd0716@gmail.com>
2024-05-29 23:25:18 +0900

    Feat: scanf

--- MyProject/printfscanf.c
+++ MyProject/printfscanf.c
@@ -24,7 +24,22 @@ int main(void)
 
 	// printf
 	// 연산
-	int add = 3 + 7; // 10
-	printf("%d x %d = %d\n", 30, 79, 30 * 79);
+	//int add = 3 + 7; // 10
+	//printf("%d x %d = %d\n", 30, 79, 30 * 79);
+
+	// scanf
+	// 키보드 입력을 받아서 저장
+	/*int input;
+	printf("값을 입력하세요 : ");
+	scanf_s("%d", &input);
+	printf("입력값 : %d\n", input);*/
+
+	int one, two, three;
+	printf("3개의 정수를 입력하세요 : ");
+	scanf_s("%d %d %d", &one, &two, &three);
+	printf("첫번째 값 : %d\n", one);
+	printf("두번째 값 : %d\n", two);
+	printf("세번째 값 : %d\n", three);
+
 	return 0;
 }
