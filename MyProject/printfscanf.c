79c383173b924353fd1421498955b75108236248
qoxow <qoxowkd0716@gmail.com>
2024-05-29 23:02:02 +0900

    Feat: 주석

--- MyProject/printfscanf.c
+++ MyProject/printfscanf.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-	int age = 12;
+	// 정수형 변수에 대한 예제
+	/*int age = 12;
 	printf("%d\n", age);
 	age = 13;
-	printf("%d\n", age);
+	printf("%d\n", age);*/
+
+	printf("1\n");
+	//printf("2\n"); // 2는 범위에서 벗어나므로 주석 처리ㅊ
+	printf("3\n");
+
 	return 0;
 }
