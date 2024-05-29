6d3ab80638d39d27c66112b2106669133a3c3620
qoxow <qoxowkd0716@gmail.com>
2024-05-29 23:06:05 +0900

    Feat: 실수형 변수

--- MyProject/printfscanf.c
+++ MyProject/printfscanf.c
@@ -7,9 +7,15 @@ int main(void)
 	age = 13;
 	printf("%d\n", age);*/
 
-	printf("1\n");
+	// 실수형 변수에 대한 예제
+	//printf("1\n");
 	//printf("2\n"); // 2는 범위에서 벗어나므로 주석 처리ㅊ
-	printf("3\n");
+	//printf("3\n");
 
+	// 실수형 변수에 대한 예제
+	float f = 46.5f;
+	printf("%.2f\n", f);
+	double d = 4.428;
+	printf("%.2lf\n", d);
 	return 0;
 }
