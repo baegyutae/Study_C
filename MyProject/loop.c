07b229923247216589669c54eb99cba55ff45214
qoxow <qoxowkd0716@gmail.com>
2024-05-31 00:32:03 +0900

    Feat: 뿔뿔

---
+++ MyProject/loop.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");*/
+
+	// ++ »Ô»Ô
+	/*int a = 10;
+	printf("a´Â %d\n", ++a);
+	a++;
+	printf("a´Â %d\n", ++a);
+	a++;
+	printf("a´Â %d\n", ++a);*/
+
+	//int b = 20;
+	//// b = b + 1;
+	//printf("b´Â %d\n", ++b);
+	//printf("b´Â %d\n", b++);
+	//printf("b´Â %d\n", b);
+
+	int i = 1;
+	printf("Hello World %d\n", i++); // 1
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	
+	return 0;
+}
