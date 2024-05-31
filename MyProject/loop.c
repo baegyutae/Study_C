6cf8d4952f00e2ee0f177fdb02cee924b00d7c9a
qoxow <qoxowkd0716@gmail.com>
2024-05-31 15:50:41 +0900

    Feat: 프로젝트2

--- MyProject/loop.c
+++ MyProject/loop.c
@@ -121,24 +121,44 @@ int main(void)
 	 *****
 	*/
 
-	for (int i = 0; i < 5; i++)
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	for (int j = i; j < 5-1; j++)
+	//	{
+	//		printf(" ");
+	//	}
+	//	for (int k = 0; k <= i; k++)
+	//	{
+	//		printf("*");
+	//	}
+	//	printf("\n");
+	//}
+
+// 피라미드를 쌓아라 - 프로젝트
+
+/*
+    *
+   ***
+  *****
+ *******
+*********
+*/
+
+int floor;
+printf("몇 층으로 쌓겠느냐?");
+scanf_s("%d", &floor);
+for (int i = 0; i < floor; i++)
+{
+	for (int j = i; j < floor - 1; j++)
 	{
-		for (int j = i; j < 5-1; j++)
-		{
-			printf(" ");
-		}
-		for (int k = 0; k <= i; k++)
-		{
-			printf("*");
-		}
-		printf("\n");
+		printf(" ");
 	}
-
-
-
-
-
-
+	for (int k = 0; k < i * 2 + 1; k++)
+	{
+		printf("*");
+	}
+	printf("\n");
+}
 
 
 
