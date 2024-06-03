a218c74d486da0559d405775939063bb8e552c84
qoxow <qoxowkd0716@gmail.com>
2024-06-03 23:07:36 +0900

    Feat: 반환값이 없는 함수

--- MyProject/function.c
+++ MyProject/function.c
@@ -3,34 +3,40 @@
 // 선언
 void p(int num);
 
+void function_without_return();
+
 int main(void)
 {
-	// functionnn
-	// 계산기
+	//// functionnn
+	//// 계산기
 
-	int num = 2;
-	//printf("num은 %d입니다.\n", num); // 2
-	p(num);
+	//int num = 2;
+	////printf("num은 %d입니다.\n", num); // 2
+	//p(num);
 
-	// 2 + 3은?
-	num = num + 3; // num += 3;
-	//printf("num은 %d입니다.\n", num); // 5
-	p(num);
+	//// 2 + 3은?
+	//num = num + 3; // num += 3;
+	////printf("num은 %d입니다.\n", num); // 5
+	//p(num);
 
-	// 5 - 1은?
-	num -= 1; // num = num -1;
-	//printf("num은 %d입니다.\n", num); // 4
-	p(num);
+	//// 5 - 1은?
+	//num -= 1; // num = num -1;
+	////printf("num은 %d입니다.\n", num); // 4
+	//p(num);
 
-	// 4 x 3은?
-	num *= 3;
-	//printf("num은 %d입니다.\n", num); // 12
-	p(num);
+	//// 4 x 3은?
+	//num *= 3;
+	////printf("num은 %d입니다.\n", num); // 12
+	//p(num);
 
-	// 12 / 6은?
-	num /= 6;
-	//printf("num은 %d입니다.\n", num); // 2
-	p(num);
+	//// 12 / 6은?
+	//num /= 6;
+	////printf("num은 %d입니다.\n", num); // 2
+	//p(num);
+
+	// 함수 종류
+	// 반환값이 없는 함수
+	function_without_return();
 
 	return 0;
 }
@@ -44,7 +50,12 @@ void p(int num)
 // 전달값 : 36
 // 함수 : ㅁ + 4
 // 반환형 : ?
-void 함수이름(int num1, int num2, char c, float f)
-{
+//void 함수이름(int num1, int num2, char c, float f)
+//{
+//
+//}
 
+void function_without_return()
+{
+	printf("반환값이 없는 함수입니다.\n");
 }
